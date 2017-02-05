9ee6c2ba73ecaffed6d4d6ad03b13109bf1e726d
Debargha Bhattacharjee <Debargha Bhattacharjee>
2017-02-05 23:42:49 +0530

    added ui

--- server.js
+++ server.js
@@ -70,12 +70,13 @@ function createTemplate (data) {
 		`<html>
 			<head>
 				<title>${title}</title>
+				<meta name="viewport" content="width-device-width, initial-scale=1" />
 				<link href="../ui/style.css" rel="stylesheet" />
 			</head>
 			<body>
 			  <div class="configuration">
 				<div>
-					<a href="../ui/index.html">Home</a>
+					<a href="/">Home</a>
 				</div>
 				<hr/>
 				<h3>${heading}</h3>
