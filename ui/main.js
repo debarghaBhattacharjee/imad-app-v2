9f5d8065bd700e09fcea5841f9566ee6e00b01ab
Debargha Bhattacharjee <debarghab.kvdima@gmail.com>
2017-02-06 01:14:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,5 +4,5 @@ console.log('Loaded!');
 alert("Hi! I am javacript");
 			  
 var element= document.getElementById('main_text');
-element.innerHtml= 'New Value!!';
+element.innerHTML= 'New Value!!';
 
