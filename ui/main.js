3fd11b6cf3575fc55f80b0fc51f18f80846e0103
Debargha Bhattacharjee <debarghab.kvdima@gmail.com>
2017-02-06 01:24:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,3 +6,8 @@ alert("Hi! I am javacript");
 var element= document.getElementById("main_text");
 element.innerHTML= 'New Value!!';
 
+var element_img= document.getElementById("img");
+element_img.onclik= function() {
+    img.style.marginLeft= '200px';
+};
+
