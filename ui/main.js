bb2999c5fa7300a16060efe262e4ac242b08f9ad
Debargha Bhattacharjee <Debargha Bhattacharjee>
2017-02-06 01:49:18 +0530

    hello

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,14 @@ var element= document.getElementById("main_text");
 element.innerHTML= 'New Value!!';
 
 var element_img= document.getElementById("elementImg");
+var marginLeft= 0;
+
+function moveRight{
+	marginLeft= marginLeft+ 5;
+    element_img.style.marginLeft= marginLeft+ "px";
+}
+
 element_img.onclick= function() {
-    element_img.style.marginRight= "400px";
+    var interval= (moveRight, 5 0);		
 };
 
