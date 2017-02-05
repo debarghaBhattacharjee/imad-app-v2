b1b1b04528e37fdb7ce270e16cbf96b7cb7126c6
Debargha Bhattacharjee <debarghab.kvdima@gmail.com>
2017-02-06 00:53:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,6 @@ alert("Hi! I am javacript");
 			  
 var element= document.getElementById('main_text');
 element.innerHtml= `<p>
-                I will change the content as and when I wish!<\br>
+                I will change the content as and when I wish!<br\>
                 The changes will be made in javascript!
 			  </p>`;
