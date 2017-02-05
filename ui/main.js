e17246df84aac9c8de2fe872645eae9c694d22e5
Debargha Bhattacharjee <Debargha Bhattacharjee>
2017-02-06 00:46:42 +0530

    Made changes for javascript

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,11 @@ console.log('Loaded!');
 
 
 alert("Hi! I am javacript");
+
+var content= `<p>
+                I will change the content as and when I wish!<\br>
+                The changes will be made in javascript!
+			  </p>`;
+			  
+var element= document.getElementById('main_text');
+element.innerHtml= ${content};
